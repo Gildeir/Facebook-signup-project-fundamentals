b75e6e0835a8854135e1ec7c5410447ffc9c3823
brunobastosfer <brunobastos@edu.unifor.br>
2021-03-23 02:11:59 -0300

    resolução da décima oitava questão

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 const getButton = document.getElementById('button-login');
 const getInputLogin = document.getElementById('user-email-phone');
+const getInputs = document.getElementsByTagName('input');
+const buttonCadastro = document.querySelector('#facebook-register');
+const getMensage = document.querySelector('#alert-mensage');
 
 getButton.addEventListener('click', () => alert(getInputLogin.value));
+
+buttonCadastro.addEventListener('click', (e) => {
+  for (let index = 0; index < getInputs.length; index += 1) {
+    if (getInputs[index].value === '') {
+      getMensage.innerHTML = 'Campos inválidos';
+    }
+  }
+  e.preventDefault();
+});
